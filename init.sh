4d2e3862ca427e5f843ca6f96e30f2245ef4ea88
kirillsev1 <kirillprinh47@gmail.com>
2023-02-14 10:11:24 +0300

    change start files

---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker volume create --name=grafana_data
+docker-compose build
--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker volume create --name=grafana_data
-docker-compose build
 docker-compose up
